430aa41c94cdffc9afb170c45795548902a3cff5
Sreenidhi S <Sreenidhi.S1@ibm.com>
2024-03-25 15:58:51 +0530

    Update test_hmc_power_system.py

--- tests/unit/modules/test_hmc_power_system.py
+++ tests/unit/modules/test_hmc_power_system.py
@@ -12,63 +12,64 @@ hmc_auth = {'username': 'hscroot', 'password': 'password_value'}
 test_data1 = [
     # All PowerOff Managed System Testdata
     # when hostname is missing
-    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': 'poweroff', 'state': None, 'system_name': "system_name"},
+    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': 'poweroff', 'state': None, 'system_name': "system_name", 'metrics': None},
      "ParameterError: mandatory parameter 'hmc_host' is missing"),
     # when system_name is missing
-    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'poweroff', 'system_name': None, 'state': None},
+    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'poweroff', 'system_name': None, 'state': None, 'metrics': None},
      "ParameterError: mandatory parameter 'system_name' is missing"),
     #when metric is provided
-    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'poweroff', 'system_name': "stsem_name, 'state': None, metric: 'metric'},
+    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'poweroff', 'system_name': "stsem_name, 'state': None, metrics: 'metric'},
      "ParameterError: unsupported parameters: metrics"),    
 
 test_data2 = [
     # All PowerOn Power system Testdata
     # when hostname is missing
-    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': 'poweron', 'state': None, 'system_name': "system_name"},
+    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': 'poweron', 'state': None, 'system_name': "system_name", 'metrics': None},
      "ParameterError: mandatory parameter 'hmc_host' is missing"),
     # when system_name is missing
-    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'poweron', 'system_name': None, 'state': None},
+    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'poweron', 'system_name': None, 'state': None, 'metrics': None},
      "ParameterError: mandatory parameter 'system_name' is missing")]
 
 test_data3 = [
     # All modify system_config data
     # when hostname is missing
-    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': 'modify_syscfg', 'state': None, 'system_name': "system_name"},
+    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': 'modify_syscfg', 'state': None, 'system_name': "system_name", 'metrics': None},
      "ParameterError: mandatory parameter 'hmc_host' is missing"),
     # when system_name is missing
-    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'modify_syscfg', 'system_name': None, 'state': None},
+    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'modify_syscfg', 'system_name': None, 'state': None, 'metrics': None},
      "ParameterError: mandatory parameter 'system_name' is missing"),
     # when requested_num_sys_huge_pages, mem_mirroring_mode, pend_mem_region_size mentioned
     ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'modify_syscfg', 'system_name': "system_name",
       'requested_num_sys_huge_pages': 'requested_num_sys_huge_pages', 'state': None, 'pend_mem_region_size': '256',
-      'mem_mirroring_mode': 'sys_firmware_only'},
+      'mem_mirroring_mode': 'sys_firmware_only', 'metrics': None},
      "ParameterError: unsupported parameters: requested_num_sys_huge_pages, mem_mirroring_mode, pend_mem_region_size")]
 
 test_data4 = [
     # All modify_hwres data
     # when hostname is missing
-    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': 'modify_hwres', 'state': None, 'system_name': "system_name"},
+    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': 'modify_hwres', 'state': None, 'system_name': "system_name", 'metrics': None},
      "ParameterError: mandatory parameter 'hmc_host' is missing"),
     # when system_name is missing
-    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'modify_hwres', 'system_name': None, 'state': None},
+    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'modify_hwres', 'system_name': None, 'state': None, 'metrics': None},
      "ParameterError: mandatory parameter 'system_name' is missing"),
     # when new_name', 'power_off_policy', 'power_on_lpar_start_policy mentiones
     ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': 'modify_hwres', 'system_name': "system_name", 'new_name': 'new_name', 'power_off_policy': 0,
-      'power_on_lpar_start_policy': 'power_on_lpar_start_policy', 'state': None, 'pend_mem_region_size': '256', 'mem_mirroring_mode': 'sys_firmware_only'},
+      'power_on_lpar_start_policy': 'power_on_lpar_start_policy', 'state': None, 'pend_mem_region_size': '256', 'mem_mirroring_mode': 'sys_firmware_only', 
+      'metrics': None},
      "ParameterError: unsupported parameters: new_name, power_on_lpar_start_policy")]
 
 test_data5 = [
     # All Facts Partition Testdata
     # when hostname is missing
-    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': None, 'state': 'facts', 'system_name': "system_name"},
+    ({'hmc_host': None, 'hmc_auth': hmc_auth, 'action': None, 'state': 'facts', 'system_name': "system_name", 'metrics': None},
      "ParameterError: mandatory parameter 'hmc_host' is missing"),
     # when system_name is missing
-    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': None, 'system_name': None, 'state': 'facts'},
+    ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': None, 'system_name': None, 'state': 'facts', 'metrics': None},
      "ParameterError: mandatory parameter 'system_name' is missing"),
     # when new_name', 'power_off_policy', 'power_on_lpar_start_policy,requested_num_sys_huge_pages, mem_mirroring_mode, pend_mem_region_size are mentioned
     ({'hmc_host': "0.0.0.0", 'hmc_auth': hmc_auth, 'action': None, 'system_name': "system_name", 'new_name': 'new_name', 'power_off_policy': 0,
       'power_on_lpar_start_policy': 'power_on_lpar_start_policy', 'state': 'facts', 'pend_mem_region_size': '256',
-      'requested_num_sys_huge_pages': 'requested_num_sys_huge_pages', 'mem_mirroring_mode': 'sys_firmware_only'},
+      'requested_num_sys_huge_pages': 'requested_num_sys_huge_pages', 'mem_mirroring_mode': 'sys_firmware_only', 'metrics': None},
      "ParameterError: unsupported parameters: new_name, power_on_lpar_start_policy, requested_num_sys_huge_pages, mem_mirroring_mode, pend_mem_region_size")]
 
  test_data6 = [
